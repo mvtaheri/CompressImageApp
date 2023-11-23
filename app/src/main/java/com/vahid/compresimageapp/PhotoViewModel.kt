f0a5795ee3908fe5f5dc4301ea9682e1aee868fd
mohammad taheri <mvtaheri@gmail.com>
2023-11-23 13:01:11 +0330

    update project

---
+++ app/src/main/java/com/vahid/compresimageapp/PhotoViewModel.kt
@@ -0,0 +1,30 @@
+package com.vahid.compresimageapp
+
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import java.util.UUID
+
+class PhotoViewModel : ViewModel() {
+    var unCompressedUri: Uri? by mutableStateOf(null)
+        private set
+    var compressedBitmap: Bitmap? by mutableStateOf(null)
+        private set
+    var workId: UUID? by mutableStateOf(null)
+        private set
+
+    fun updateUnCompressUri(uri: Uri) {
+        unCompressedUri = uri
+    }
+
+    fun updateCompressBitmap(bitmap: Bitmap?) {
+        compressedBitmap = bitmap
+    }
+
+    fun updateWorkId(uuid: UUID?) {
+        workId = uuid
+    }
+}
